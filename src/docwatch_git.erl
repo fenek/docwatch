e11365df477e76101131db69c1e7a4becabc47cc
Iuri L. Machado <iuri.machado@erlang-solutions.com>
2021-03-30 10:21:19 -0300

    Filters git file output depending..

--- src/docwatch_git.erl
+++ src/docwatch_git.erl
@@ -1,11 +1,15 @@
 -module(docwatch_git).
 
--export([is_git_installed/0, get_changed_files/1]).
+-export([is_git_installed/0, get_changed_files/2]).
 
-get_changed_files(Path) ->
+get_changed_files(Path, Main) ->
   case is_git_installed() of
     true ->
-      os:cmd("cd " ++ Path ++ " && git diff --name-only HEAD");
+      Result = os:cmd("cd " ++ Path ++ " && git diff --name-only " ++ Main),
+      Trimmmed = string:trim(Result, both),
+      FileNames = string:split(Trimmmed, "\n", all),
+      FilesThatMatter = lists:filter(fun filter_files_that_matter/1, FileNames),
+      {ok, FilesThatMatter};
     false ->
       {error, "Git not found"}
   end.
@@ -16,4 +20,10 @@ is_git_installed() ->
       false;
     _ ->
       true
+  end.
+
+filter_files_that_matter(FileName) ->
+  case re:run(FileName, "\.(erl|hrl|md)") of
+    nomatch -> false;
+    {match, _} -> true
   end.
